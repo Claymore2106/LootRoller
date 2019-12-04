77a2d16caba07844d6522ef4c940c122d89555b8
Nicholas Dechert <claymore2106@gmail.com>
2019-12-04 04:25:03 -0800

    Remove my email from the .bat and .sh files, as it isn't necessary

--- lootroller.bat
+++ lootroller.bat
@@ -2,6 +2,6 @@ python lootroller.py
 @echo off
 ECHO.
 ECHO.
-ECHO Hopefully finished without error. Please report any errors to the project on GitHub or Claymore2106@gmail.com
+ECHO Hopefully finished without error. Please report any errors to the project on GitHub
 ECHO.
 pause
--- lootroller.sh
+++ lootroller.sh
@@ -2,5 +2,5 @@
 
 python3 lootroller.py
 
-read -p $'\n\nHopefully finished without error. Please report any errors to the project on GitHub or Claymore2106@gmail.com'
+read -p $'\n\nHopefully finished without error. Please report any errors to the project on GitHub'
 
